370de4fba702c2e91006fed07d569794f40d746e
KoskeK <koske.kanasugi@truhla.eu>
2026-09-13 11:35:10 +0300

    stoopid

--- misc/generate_password.py
+++ misc/generate_password.py
@@ -1,9 +1,9 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 
-input("Generate or check? (g/c)> ")
-if input == "g":
+answer = input("Generate or check? (g/c)> ")
+if answer == "g":
     print(generate_password_hash(input("Input password> ")))
-if input == "c":
+if answer == "c":
     print(check_password_hash(input("Hash> "), input("Password> ")))
 else:
     print("Invalid input")
