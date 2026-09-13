0098cf5d29b064cee7dfd31c18d733c0651409d7
KoskeK <koske.kanasugi@truhla.eu>
2026-09-13 16:27:41 +0300

    login required

--- src/koske_galary/main.py
+++ src/koske_galary/main.py
@@ -134,6 +134,7 @@ def login():
     return render_template('login.html')
 
 @app.route("/manage", methods=["GET", "POST"])
+@login_required
 def manage():
     if request.method == "POST":
         username = request.form.get('username')
